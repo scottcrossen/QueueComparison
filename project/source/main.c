4c90c9fe2e104087f99822f55fc68346fef6a255
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-10-17 00:30:09 -0600

    made memory mapped queue dynamic

--- project/source/main.c
+++ project/source/main.c
@@ -9,44 +9,44 @@
 #include "mmqueue.h"
 
 int main(int argc, char **argv) {
-  printf("%s\r\n", "****************");
-  printf("%s\r\n", "Program starting");
+  printf("****************\r\n");
+  printf("Program starting\r\n");
   linked_queue_t* linked_queue = createLinkedQueue(1/*getpagesize()*/);
   array_queue_t* array_queue = createArrayQueue(1/*getpagesize()*/);
-  mm_queue_t* mm_queue = createMMQueue(getpagesize());
-  printf("%s\r\n", "Queues created");
-
-
+  mm_queue_t* mm_queue = createMMQueue(1/*getpagesize()*/);
+  printf("Queues created\r\n");
   char a;
+  printf("Testing linked list queue\r\n");
   enqueueLinked(linked_queue, 'a');
   enqueueLinked(linked_queue, 'b');
+  dequeueLinked(linked_queue, &a); printf("%c\r\n", a);
   enqueueLinked(linked_queue, 'c');
   dequeueLinked(linked_queue, &a); printf("%c\r\n", a);
   dequeueLinked(linked_queue, &a); printf("%c\r\n", a);
   dequeueLinked(linked_queue, &a); printf("%c\r\n", a);
+  printf("Testing array queue\r\n");
   enqueueArray(array_queue, 'a');
   enqueueArray(array_queue, 'b');
+  dequeueArray(array_queue, &a); printf("%c\r\n", a);
   enqueueArray(array_queue, 'c');
   dequeueArray(array_queue, &a); printf("%c\r\n", a);
   dequeueArray(array_queue, &a); printf("%c\r\n", a);
   dequeueArray(array_queue, &a); printf("%c\r\n", a);
+  printf("Testing memory mapped queue\r\n");
   enqueueMM(mm_queue, 'a');
   enqueueMM(mm_queue, 'b');
+  dequeueMM(mm_queue, &a); printf("%c\r\n", a);
   enqueueMM(mm_queue, 'c');
   dequeueMM(mm_queue, &a); printf("%c\r\n", a);
   dequeueMM(mm_queue, &a); printf("%c\r\n", a);
   dequeueMM(mm_queue, &a); printf("%c\r\n", a);
-  dequeueMM(mm_queue, &a); printf("%c\r\n", a);
-
-
-  printf("%s\r\n", "Deleting queues");
+  printf("Deleting queues\r\n");
   destroyLinkedQueue(linked_queue);
   destroyArrayQueue(array_queue);
   destroyMMQueue(mm_queue);
-  printf("%s\r\n", "Program finished");
-  printf("%s\r\n", "****************");
+  printf("Program finished\r\n");
+  printf("****************\r\n");
 }
-
 
 double now() {
     struct timeval tp;
--- project/source/mmqueue.c
+++ project/source/mmqueue.c
@@ -9,10 +9,14 @@
 #include <errno.h>
 #include <string.h>
 
-mm_queue_t* createMMQueue(unsigned int req_size) {
+void resizeMM(mm_queue_t* this);
+
+mm_queue_t* createMMQueue(unsigned int size) {
   mm_queue_t* this = malloc(sizeof(mm_queue_t));
   int page_size = getpagesize();
-  this->allocated = (req_size * sizeof(char) / page_size + ((req_size * sizeof(char)) % page_size != 0)) * page_size;
+  int obj_size = sizeof(char);
+  this->allocated = (size * obj_size / page_size + ((size * obj_size) % page_size != 0)) * page_size;
+  this->size = size;
   if ((this->file_descriptor = shm_open("queue_region", O_RDWR|O_CREAT, 0600)) == -1) {
     printf("Error on obtaining anonymous file: %s\r\n", strerror(errno));
   }
@@ -23,22 +27,22 @@ mm_queue_t* createMMQueue(unsigned int req_size) {
     printf("Could not allocate virtual memory: %s\r\n", strerror(errno));
   }
   if(mmap(this->buffer, this->allocated, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, this->file_descriptor, 0) == MAP_FAILED){
-    printf("Could not map buffer into virtual memory: %s\r\n", strerror(errno));
+    printf("Could not map first buffer into virtual memory: %s\r\n", strerror(errno));
   }
-  if(mmap(this->buffer + this->allocated, this->allocated, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, this->file_descriptor, 0) == MAP_FAILED){
-    printf("Could not map buffer into virtual memory: %s\r\n", strerror(errno));
+  if(mmap(this->buffer + this->allocated / obj_size, this->allocated, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, this->file_descriptor, 0) == MAP_FAILED){
+    printf("Could not map second buffer into virtual memory: %s\r\n", strerror(errno));
   }
-  this->size = this->allocated / sizeof(char);
   this->head = 0;
   this->tail = 0;
   return this;
 }
 
 void destroyMMQueue(mm_queue_t* this) {
-  if(munmap(this->buffer + this->allocated, this->allocated) != 0){
+  int obj_size = sizeof(char);
+  if(munmap(this->buffer, this->allocated) != 0){
     printf("Could not unmap buffer: %s\r\n", strerror(errno));
   }
-  if(munmap(this->buffer, this->allocated) != 0){
+  if(munmap(this->buffer + this->allocated / obj_size, this->allocated) != 0){
     printf("Could not unmap buffer: %s\r\n", strerror(errno));
   }
   if(close(this->file_descriptor) != 0){
@@ -48,10 +52,9 @@ void destroyMMQueue(mm_queue_t* this) {
 
 void enqueueMM(mm_queue_t* this, char data) {
   if (this->tail - this->head >= this->size) {
-    printf("Warning: Queue is not dynamic. Insert ignored.\r\n");
-    return;
+    resizeMM(this);
   }
-  *(this->buffer+(++this->tail % (2 * this->size))) = data;
+  *(this->buffer+(++this->tail % (2 * this->allocated))) = data;
 }
 
 void dequeueMM(mm_queue_t* this, char* data) {
@@ -59,5 +62,41 @@ void dequeueMM(mm_queue_t* this, char* data) {
     *(data) = 0;
     return;
   }
-  *(data) = *(this->buffer+(++this->head % (2 * this->size)));
+  *(data) = *(this->buffer+(++this->head % (2 * this->allocated)));
+}
+
+void resizeMM(mm_queue_t* this) {
+  int obj_size = sizeof(char);
+  unsigned int new_size = this->size * 2 * obj_size;
+  if (new_size >= this->allocated / sizeof(char)) {
+    // Warning:  I haven't tested this segment of code.
+    unsigned int new_allocated = this->allocated * 2;
+    if (ftruncate(this->file_descriptor, new_allocated) != 0){
+      printf("Could not resize anonymous file: %s\r\n", strerror(errno));
+    }
+    char* new_buffer;
+    if((new_buffer = (char *) mmap(NULL, 2 * new_allocated, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)) == MAP_FAILED) {
+      printf("Could not reallocate virtual memory: %s\r\n", strerror(errno));
+    }
+    if(mmap(new_buffer, new_allocated, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, this->file_descriptor, 0) == MAP_FAILED) {
+      printf("Could not map first buffer into virtual memory: %s\r\n", strerror(errno));
+    }
+    if(mmap(new_buffer + new_allocated / obj_size, new_allocated, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, this->file_descriptor, 0) == MAP_FAILED) {
+      printf("Could not map second buffer into virtual memory: %s\r\n", strerror(errno));
+    }
+    unsigned int new_tail = new_size;
+    unsigned int new_head = new_size;
+    while (this->head < this->tail) {
+      *(new_buffer + (++new_tail)) = *(this->buffer+(++this->head % (2 * this->allocated)));
+    }
+    if(munmap(this->buffer, this->allocated) != 0){
+      printf("Could not unmap buffer: %s\r\n", strerror(errno));
+    }
+    if(munmap(this->buffer + this->allocated / obj_size, this->allocated) != 0){
+      printf("Could not unmap buffer: %s\r\n", strerror(errno));
+    }
+    this->head = new_head;
+    this->tail = new_tail;
+  }
+  this->size = new_size;
 }
